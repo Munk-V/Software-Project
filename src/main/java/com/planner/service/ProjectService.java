0ac86e33a80fa134395058b02ce1c94e3b9ca106
VedantaSinha <sinha.vedanta@gmail.com>
2026-05-07 13:28:01 +0200

    Update ProjectService.java

--- src/main/java/com/planner/service/ProjectService.java
+++ src/main/java/com/planner/service/ProjectService.java
@@ -4,9 +4,9 @@ package com.planner.service;
 import java.util.List;
 
 import com.planner.domain.Developer;
-import com.planner.domain.Project;  
-import com.planner.repository.DeveloperRepository;
-import com.planner.repository.ProjectRepository;
+import com.planner.domain.Project;
+import com.planner.repository.IDeveloperRepository;
+import com.planner.repository.IProjectRepository;
 
 public class ProjectService { //LOgic 
 
