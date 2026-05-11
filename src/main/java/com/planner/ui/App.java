78d0c9b6534ff2e9a7ceb3d4690dfef26171268f
Nat-skov <nat.skovlund2004@gmail.com>
2026-05-11 20:53:30 +0200

    Update App.java

--- src/main/java/com/planner/ui/App.java
+++ src/main/java/com/planner/ui/App.java
@@ -1,6 +1,6 @@
 package com.planner.ui;
 // Nat
-
+//The Part that starts the UI (and therfor the program)
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,11 +10,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         AppContext context = new AppContext();
+        //INstantiates the REpocitoryes needet by the UI 
         MainWindow mainWindow = new MainWindow(context);
         Scene scene = new Scene(mainWindow.getRoot(), 900, 600);
         stage.setTitle("Software Project Planner");
         stage.setScene(scene);
         stage.show();
+        // Makes the STart window
     }
 
     public static void main(String[] args) {
